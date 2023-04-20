0055ad3b29181ac1b225865de21d5d92f2e9f511
Mahesh Menon <menontmahesh@gmail.com>
2023-04-20 16:51:57 -0400

    added main file speech - not tested tho

--- mrpose.py
+++ mrpose.py
@@ -1,6 +1,8 @@
 import argparse
 import subprocess
 from utils.exercise_utils import Exercise
+from PIL import Image
+import time
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -21,16 +23,27 @@ if __name__ == '__main__':
     python27_path = "C:\Python27\python.exe  naoqi_demo.py"
     subprocess.run("C:\Python27\python.exe  headTouch.py")
     subprocess.run(python27_path + " --movement wake_up")
-    subprocess.run(python27_path + " --speech \"“Hi my name is Jaunty and I’m a NAO robot. Today I will be assisting you with pushups Tap my head when you’re ready to start”\"")
+
+    intro_speech = "Hi my name is Jaunty and today I’ll lead you through a small workout. We’re going to be doing 5 pushups. To do a pushup, lay on the ground, raise your torso off the ground, and move your arms back and forth like this."
+    subprocess.run(python27_path + " --speech "+intro_speech + " --movement push_up")
     subprocess.run("C:\Python27\python.exe  headTouch.py")
 
-    subprocess.run(python27_path + " --speech \"Do five pushups with me! To do a pushup, get on the ground, raise your torso off the ground, and move your arms back and forth like this.\" --movement pushup")
+    pre_pushup = "Now it’s your turn. Start whenever you’re ready and I’ll count down your reps. After your 5th push up, enter a plank position. You’ll now be shown a picture of what a proper plank looks like."
+    subprocess.run(python27_path + " --speech "+pre_pushup)
 
+    img = Image.open("plank.jpeg")
+    img.show()
+    time.sleep(5)
+    img.close()
 
     pose = Exercise(video, "pushup")
     pose.estimate_exercise(encouragement)
+
+    pre_plank = "You’ve completed 5 pushups, good job! Once my head is tapped by one of the experiment conductors, I’ll start to time your plank. Please notify one of the experiment conductors when you’re ready to start your plank!"
+    subprocess.run("C:\Python27\python.exe  headTouch.py")
+
     pose = Exercise(video, "plank")
     pose.estimate_exercise(encouragement)
 
-    # subprocess.run(python27_path + " --speech \"Good job! You did great! I hope I was able to help you with exercise today!\"")
-    # subprocess.run(python27_path + " --movement rest")
+    subprocess.run(python27_path + " --speech \"That’s all! Thank you for participating in this round of the experiment\"")
+    subprocess.run(python27_path + " --movement rest")
